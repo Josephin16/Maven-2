425b6844cebeafcf856f4e3da8914d8a7b789f76
Josephin16 <josephin.c96@gmail.com>
2019-12-13 08:15:42 +0530

    pulled

--- src/test/java/test/cts/Master.java
+++ src/test/java/test/cts/Master.java
@@ -7,6 +7,7 @@ public class Master {
 System.out.println("Test Maven - Master");
 System.out.println("Test Maven - Branch-2");
 System.out.println("Pulled-B2");
+System.out.println("Pull");
 	}
 
 }
