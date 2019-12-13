0dbf9a57d3af58283c50d6b31e9b18f8eba65982
Josephin16 <josephin.c96@gmail.com>
2019-12-13 08:39:23 +0530

    updated B 2

--- src/test/java/test/cts/Master.java
+++ src/test/java/test/cts/Master.java
@@ -10,6 +10,7 @@ System.out.println("Pulled-B2");
 System.out.println("Test Maven - Branch1");
 System.out.println("Not an updated copy");
 System.out.println("Pull");
+System.out.println("Raj");
 
 	}
 
